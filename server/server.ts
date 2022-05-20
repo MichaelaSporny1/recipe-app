489b13ddeaf598f210c0b15f56e96de617e9e28a
MichaelaSporny1 <michaela.sporny@iths.se>
2022-05-20 14:20:40 +0200

    try fixing .env issues

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,9 @@ dotenv.config();
 
 console.log(process.env.MONGO_DB_CONNECTION_STRING);
 
-// connect('mongodb+srv://michaela:tyEVJwjCrzGq1Ws8@cluster0.iux78.mongodb.net/recipes?retryWrites=true&w=majority')
+connect('mongodb+srv://michaela:tyEVJwjCrzGq1Ws8@cluster0.iux78.mongodb.net/recipes?retryWrites=true&w=majority')
+// connect(process.env.MONGO_DB_CONNECTION_STRING);
+
 
 // connect('mongodb://localhost:27017/recipes')
 
@@ -23,13 +25,13 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-if (process.env.MONGO_DB_CONNECTION_STRING) {
-    connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
-        app.listen(port, () => console.log('listening to port: ' + port));
-    })
-} else {
-    console.log('Configuration MONGO_DB_CONNECTION_STRING not set')
-}
+// if (process.env.MONGO_DB_CONNECTION_STRING) {
+//     connect(process.env.MONGO_DB_CONNECTION_STRING).then(() => {
+//         app.listen(port, () => console.log('listening to port: ' + port));
+//     })
+// } else {
+//     console.log('Configuration MONGO_DB_CONNECTION_STRING not set')
+// }
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
